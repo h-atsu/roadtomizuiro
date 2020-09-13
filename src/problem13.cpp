0a6ee9b0bc3aff1052c857c1b22d99dbc09ca772
h-atsu <jhuim08@gmail.com>
2020-09-13 23:53:49 +0900

    today's progress

--- src/problem13.cpp
+++ src/problem13.cpp
@@ -24,9 +24,39 @@ const double PI=3.14159265358979323846;
 int dx[] = {1,-1,0,0};
 int dy[] = {0,0,1,-1};
 
+int senbei[13][10010];
 
 int main(){
+    int R,C;
+    cin >> R >> C;
+    rep(i,R) {
+	rep(j,C) {
+	    cin >> senbei[i][j];
+	}
+    }
+    int ans = 0;
+    
+    for(int bit =0; bit < (1<<R); bit++) {
+	int tmp = 0;
+	rep(i,C) {
+	    int back = 0;
+	    int front = 0;
+	    rep(j,R) {
+		int now = senbei[j][i];
+		if(bit & (1 << j)) now = 1 - now;
+		if(now == 0) back++;
+		else front++;
+	    }
+	    tmp += max(back,front);
+	}
+	ans = max(tmp,ans);
+    }
+    cout << ans << endl;
 
-  return 0;
+
+
+    
+    
+    return 0;
 }
 
--- src/problem23.cpp
+++ src/problem23.cpp
@@ -26,7 +26,69 @@ int dy[] = {0,0,1,-1};
 
 
 int main(){
+    ll N,M;
+    cin >> N >> M;
+    N++;
+    ll P[N];
+    P[N-1] = 0;
+    rep(i,N-1) {
+	cin >> P[i];
+    }
+    sort(P,P+N);
 
-  return 0;
+    ll R[N*N];
+    rep(i,N) {
+	rep(j,N) {
+	    R[i*N + j] = P[i] + P[j];
+	}
+    }
+    sort(R,R+N*N);
+
+    
+    ll ans = 0;
+    rep(i,N*N) {
+	ll rest = M - R[i];
+	if(rest < 0) break;
+	auto key = upper_bound(R,R+N*N,rest) - R - 1;
+	rest -= R[key];
+	ans = max(ans,M-rest);
+    }
+    cout << ans << endl;
+    // for(auto itr : P) cout << itr << " ";
+    // cout << endl;
+    return 0;
 }
+
+
+
+
+
+
+// O(N^3logN)解放 想定解にするにはO(N^2logN)にする必要がある
+// int main(){
+//     ll N,M;
+//     cin >> N >> M;
+//     N++;
+//     ll P[N];
+//     P[N-1] = 0;
+//     rep(i,N-1) {
+// 	cin >> P[i];
+//     }
+//     sort(P,P+N);
+
+//     ll ans = 0;
+//     rep(i,N) {
+// 	rep(j,N) {
+// 	    rep(k,N) {
+// 		ll rest = M - (P[i] + P[j] + P[k]);
+// 		if(rest < 0) break;
+// 		int key = upper_bound(P,P+N,rest) - P - 1;
+// 		rest -= P[key];
+// 		ans = max(ans,M - rest);
+// 	    }
+// 	}
+//     }
+//     cout << ans << endl;
+//     return 0;
+// }
 
