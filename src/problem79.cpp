d162bcdc1a7ebcd35b4a638be2d0321c9e446580
h-atsu <jhuim08@gmail.com>
2020-10-02 23:29:01 +0900

    進捗orz

--- src/problem79.cpp
+++ src/problem79.cpp
@@ -25,8 +25,86 @@ int dx[] = {1,-1,0,0};
 int dy[] = {0,0,1,-1};
 
 
-int main(){
+vector<vector<int>> table(550,vector<int>(550,0));
+vector<vector<int>> cum(550,vector<int>(550,0));
+
+int main() {
+    int N,M,Q;
+    cin >> N >> M >> Q;
+    rep(i,M) {
+	int l,r;
+	cin >> l >> r;
+	l--,r--;
+	table[l][r]++;
+    }
+    rep(i,N) {
+	rep(j,N) {
+	    cum[i+1][j+1] = table[i][j] + cum[i+1][j];
+	}
+    }
+
+    int ans[Q];
+
+    rep(i,Q) {
+	int p,q;
+	cin >> p >> q;
+	int tmp=0;
+	for(int j=p; j<=q; j++) {
+	    tmp += cum[j][q] - cum[j][p-1];
+	}
+	ans[i] = tmp;
+    }
+
+    // rep(i,N+1) {
+    // 	rep(j,N+1) cout << cum[i][j];
+    // 	cout << endl;
+    // }
     
-    return 0;
+    rep(i,Q) cout << ans[i] << endl;
 }
+
+
+
+//　この方法だと漏れがある
+// int main(){
+//     int N,M,Q;
+//     cin >> N >> M >> Q;
+//     rep(i,M) {
+// 	int l,r;
+// 	cin >> l >> r;
+// 	L[l]++;
+// 	R[r]++;
+//     }
+//     rep(i,N) {
+// 	cuml[i+1] = cuml[i] + L[i+1];
+// 	cumr[i+1] = cumr[i] + R[i+1];
+//     }
+
+//     // for(int i=0; i<=N; i++) {
+//     // 	cout << cuml[i] << cumr[i] << endl;
+//     // }
+//     int ans[Q];
+    
+//     rep(i,Q) {
+// 	int p,q;
+// 	cin >> p >> q;
+// 	int le = M - cuml[p-1];
+// 	int ri = cumr[q];
+// 	ans[i] = min(ri,le);
+//     }
+//     cout << "---------------------" << endl;
+//     rep(i,M) cout << cuml[i] << " " << cumr[i] << endl;
+
+//     cout << "---------------------" << endl;
+//    for(auto itr : ans) cout << itr << endl;
+
+
+
+    
+
+
+
+    
+//     return 0;
+// }
 
--- src/problem80.cpp
+++ src/problem80.cpp
@@ -26,7 +26,53 @@ int dy[] = {0,0,1,-1};
 
 
 int main(){
+    int H,W;
+    ll K,V;
+    cin >> H >> W >> K >> V;
+    ll A[200][200];
+    ll cum[200][200];
+    
+    rep(i,H) {
+	rep(j,W) {
+	    cin >> A[i][j];
+	}
+    }
 
-  return 0;
+    rep(i,H) {
+	rep(j,W) {
+	    cum[i+1][j+1] = A[i][j] + cum[i][j+1] + cum[i+1][j] - cum[i][j];
+	}
+    }
+
+
+    ll ans = 0;
+    int z=0;
+
+    for(int a=1; a<=H; a++) {
+	for(int c = a; c<=H; c++) {
+	    for(int b = 1; b<=W; b++) {
+		for(int d = b; d<=W; d++) {
+		    ll num = ll(c-a+1)*ll(d-b+1);
+		    ll val = cum[c][d] - cum[a-1][d] - cum[c][b-1] + cum[a-1][b-1];
+		    ll tmp = num * K + val;
+		    if(tmp <= V) ans = max(num,ans);
+		}
+	    }
+	}
+    }
+
+
+    cout << ans << endl;
+
+
+    // rep(i,H+1) {
+    // 	rep(j,W+1) {
+    // 	    cout << cum[i][j] << "  ";       
+    // 	}
+    // 	cout << endl;
+    // }
+    
+    
+    return 0;
 }
 
